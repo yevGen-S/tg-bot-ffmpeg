e54d613ef9f6da3b998dc533bdf4c4459e2bd538
Yevgeniy Sudakov <yevgeniysudakov@gmail.com>
2023-01-30 21:53:52 +0300

    feat: realize merge video and audio, looping video for audio duration

--- src/func3_jenok/func3.py
+++ src/func3_jenok/func3.py
@@ -2,21 +2,25 @@ import ffmpeg
 
 
 def load_video(name):
-    input_video = ffmpeg.input(f'./assets/videofiles/{name}')
+    input_video = ffmpeg.input(f'../input_videos/{name}')
     return input_video
 
 
 def load_audio(name):
-    input_audio = ffmpeg.input(f'./assets/audiofiles/{name}')
+    input_audio = ffmpeg.input(f'../input_audios/{name}')
     return input_audio
 
 
 def merge_video_and_audio(video_name, audio_name, output_name):
     try:
         command = (ffmpeg
-            .concat(load_video(video_name), load_audio(audio_name), v=1, a=1)
-            .output(f'./assets/processed/{output_name}')
-            .run())
+                   .concat(load_video(video_name), load_audio(audio_name), v=1, a=1)
+                   .output(f'../processed/{output_name}')
+                   )
+        command.run()
     except:
         err_msg = "Something go wrong! Video wasn't merged with audio"
         return err_msg
+
+
+merge_video_and_audio('test.mp4', 'gadza.mp3', 'test-gadza.mp4')
